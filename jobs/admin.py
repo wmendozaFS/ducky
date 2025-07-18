95a7f74e650630adde6d414498623db555d2e1b0
Wilmer Mendoza <wmendozar.cief.pow@gmail.com>
2025-07-18 13:18:47 +0200

    en admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,18 +1,12 @@
 
 from django.contrib import admin
-from .models import JobOffer, JobApplication, StatusMessageTemplate, Candidatura
+from .models import JobOffer, StatusMessageTemplate, Candidatura
 
 @admin.register(JobOffer)
 class JobOfferAdmin(admin.ModelAdmin):
     list_display = ['title', 'company_name', 'created_by', 'is_active', 'created_at']
     list_filter = ['is_active', 'modality']
     search_fields = ['title', 'company_name', 'created_by__username']
-    
-
-@admin.register(JobApplication)
-class JobApplicationAdmin(admin.ModelAdmin):
-    list_display = ['offer', 'applicant', 'applied_at']
-    search_fields = ['offer__title', 'applicant__username']
 
 @admin.register(StatusMessageTemplate)
 class StatusMessageTemplateAdmin(admin.ModelAdmin):
